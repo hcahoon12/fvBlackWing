76312207cd63210597284d4c7efc111a52e2b0f9
Bryce Wyzlic <bwyzlic@students.inghamisd.org>
2018-03-12 10:04:08 -0400

    Changes

---
+++ BlackWing/BlackWing/fvBlackWing/BlackWing/BlackWing/Star.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace BlackWing
+{
+    class Star
+    {
+        Texture2D startexture;
+
+        public Star()
+        {
+            List<int> starlist = new List<int>();
+        }
+        public void LoadContent(ContentManager Content)
+        {
+            startexture = Content.Load<Texture2D>("starrrr");
+        }
+        public void Update(KeyboardState keyState)
+        {
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
